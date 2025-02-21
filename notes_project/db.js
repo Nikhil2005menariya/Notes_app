79888eee452253cb5630046d225ce66447b850a5
Nikhil2005menariya <152401397+Nikhil2005menariya@users.noreply.github.com>
2025-02-21 23:12:31 +0530

    Update db.js

--- notes_project/db.js
+++ notes_project/db.js
@@ -3,7 +3,7 @@ import { MongoClient } from "mongodb";
 let dbconnection;
 
 export const connectToDb = (cb) => {
-    MongoClient.connect('mongodb://localhost:27017/notesapp')
+    MongoClient.connect('mongodb+srv://nikhilmenariya78:KyyDnC4S4Hw2iRDL@cluster0.0uazh.mongodb.net/')
         .then((client) => {
             dbconnection = client.db();
             return cb();
